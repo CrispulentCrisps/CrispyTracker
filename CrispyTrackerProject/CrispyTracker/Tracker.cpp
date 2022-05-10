0210edee9e7abaa6b1e10b2ab98e06e2c3e59fbb
CrispulentCrisps <70386744+CrispulentCrisps@users.noreply.github.com>
2022-05-10 22:09:24 +0100

    SDL conversion complete
    The windows sound API is no longer used. Will be adding polyphony next

--- CrispyTrackerProject/CrispyTracker/Tracker.cpp
+++ CrispyTrackerProject/CrispyTracker/Tracker.cpp
@@ -31,16 +31,6 @@ void MyAudioCallback(void* userdata, Uint8* stream, int len)
 
 void Tracker::Run()
 {
-	SDL_memset(&want, 0, sizeof(want)); /* or SDL_zero(want) */
-	want.freq = 48000;
-	want.format = AUDIO_F32;
-	want.channels = 2;
-	want.samples = 4096;
-	want.callback = MyAudioCallback;  // you wrote this function elsewhere.
-	want.userdata = &SG;
-	dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
-	const char* err = SDL_GetError();
-
 	bool PlayingTrack = false;
 	bool WindowIsGood = true;
 	//Initialize SDL
@@ -52,7 +42,7 @@ void Tracker::Run()
 	else
 	{
 		//Create window
-		window = SDL_CreateWindow("CrispyTracker", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		window = SDL_CreateWindow("CrispyTracker", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE);
 		if (window == NULL)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -64,6 +54,17 @@ void Tracker::Run()
 		}
 	}
 
+	SDL_memset(&want, 0, sizeof(want)); /* or SDL_zero(want) */
+	want.freq = 48000;
+	want.format = AUDIO_F32;
+	want.channels = 2;
+	want.samples = 4096;
+	want.callback = MyAudioCallback;  // you wrote this function elsewhere.
+	want.userdata = &SG;
+	dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
+	const char* err = SDL_GetError();
+
+
 	while (running) {
 		if (WindowIsGood) {
 			//Get window surface
@@ -74,7 +75,6 @@ void Tracker::Run()
 
 			//Update the surface
 			SDL_UpdateWindowSurface(window);
-			printf("WINDOW UPDATING \n");
 		}
 		CheckInput();
 	}
@@ -103,84 +103,86 @@ void Tracker::CheckInput()
 			case SDL_QUIT:
 				running = false;
 				break;
+				//upper octave
 			case SDLK_q:
 				SG.NoteIndex = TuninOff;
 				printf("PRESSED Q");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_2:
 				SG.NoteIndex = TuninOff + 1;
 				printf("PRESSED 1");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_w:
 				SG.NoteIndex = TuninOff + 2;
 				printf("PRESSED W");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_3:
 				SG.NoteIndex = TuninOff + 3;
 				printf("PRESSED 3");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_e:
 				SG.NoteIndex = TuninOff + 4;
 				printf("PRESSED E");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_r:
 				SG.NoteIndex = TuninOff + 5;
 				printf("PRESSED R");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_5:
 				SG.NoteIndex = TuninOff + 6;
 				printf("PRESSED 5");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_t:
 				SG.NoteIndex = TuninOff + 7;
 				printf("PRESSED T");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_6:
 				SG.NoteIndex = TuninOff + 8;
 				printf("PRESSED 6");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_y:
 				SG.NoteIndex = TuninOff + 9;
 				printf("PRESSED Y");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_7:
 				SG.NoteIndex = TuninOff + 10;
 				printf("PRESSED 7");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_u:
 				SG.NoteIndex = TuninOff + 11;
 				printf("PRESSED U");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			case SDLK_i:
 				SG.NoteIndex = TuninOff + 12;
 				printf("PRESSED I");
-				SG.PlayingNoise == true;
+				SG.PlayingNoise = true;
 				break;
 			}
 
-			{
-				SDL_PauseAudioDevice(dev, 0);
-				SDL_Delay(1000);
-				SDL_CloseAudioDevice(dev);
-			}
+			SDL_PauseAudioDevice(dev, 0);
+			SDL_Delay(1);
 
 			break;
+		case SDL_KEYUP:
+			SG.PlayingNoise = false;
+			SDL_PauseAudioDevice(dev, 1);
+			break;
+
 		case SDL_QUIT:
 			running = false;
 			break;
 		}
-		SG.PlayingNoise = false;
 	}
 }
